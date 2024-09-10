e577def12f4ddf9194376dc20e6b59d1f95cb3f7
Igor Ferreira <igornvf@gmail.com>
2024-09-09 23:39:02 -0300

    Adicionado campo de pesquisa e paginação no extrato.

--- src/app/transferencia-extrato/transferencia-extrato.component.ts
+++ src/app/transferencia-extrato/transferencia-extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TransferenciaService, Transferencia } from '../service/transferencia.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,6 +7,11 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-transferencia-extrato',
@@ -16,7 +21,12 @@ import { MatTableDataSource } from '@angular/material/table';
     HttpClientModule,
     MatListModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   templateUrl: './transferencia-extrato.component.html',
   styleUrls: ['./transferencia-extrato.component.scss'],
@@ -25,6 +35,10 @@ import { MatTableDataSource } from '@angular/material/table';
 export class TransferenciaExtratoComponent implements OnInit {
   displayedColumns: string[] = ['contaOrigem', 'contaDestino', 'valor', 'dataTransferencia', 'taxa'];
   transferencias = new MatTableDataSource<Transferencia>();
+  searchKey: string = '';
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private transferenciaService: TransferenciaService, private router: Router) {}
 
@@ -34,12 +48,22 @@ export class TransferenciaExtratoComponent implements OnInit {
 
   carregarExtrato() {
     this.transferenciaService.listarTransferencias().subscribe({
-      next: (data) => this.transferencias.data = data,
+      next: (data) => {
+        this.transferencias.data = data;
+        this.transferencias.paginator = this.paginator;
+        this.transferencias.sort = this.sort;
+      },
       error: (err) => console.error('Erro ao carregar o extrato', err)
     });
   }
 
   navegarParaMenu() {
     this.router.navigate(['/']);
+  }
+
+  // Método para aplicar filtro de pesquisa
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.transferencias.filter = filterValue.trim().toLowerCase();
   }
 }
